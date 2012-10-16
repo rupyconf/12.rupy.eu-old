dd5f622d18471a8d11a4ca35abaac6e93bc8954f
Fabio Rodrigues <fabio.info@gmail.com>
2012-10-16 14:37:44 -0300

    migrate fixed

--- db/migrate/20121016144937_add_speaker2_to_schedule.rb
+++ db/migrate/20121016144937_add_speaker2_to_schedule.rb
@@ -1,6 +1,6 @@
 class AddSpeaker2ToSchedule < ActiveRecord::Migration
   def self.up
-    #remove_column :schedules, :speaker_id
+    remove_column :schedules, :speaker_id
     add_column :schedules, :speaker1_id, :integer
     add_column :schedules, :speaker2_id, :integer
   end
